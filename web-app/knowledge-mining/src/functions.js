1bbecb8c04cdb0d7b8910128556c115ab2424a71
malwinq <malwina.kubas@gmail.com>
2020-12-16 14:22:11 +0100

    Remove unused functions

--- web-app/knowledge-mining/src/functions.js
+++ web-app/knowledge-mining/src/functions.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-// tbd - get all options
-
+// to be replaced - mock
 export const getDocuments = (inputText) => {
     if (inputText.includes("s")) {
         return [
@@ -88,90 +87,3 @@ export const getDocuments = (inputText) => {
         return null;
     }
 }
-
-export const getOptions = () => {
-    return [
-        {
-            label: 'szkolnictwo: Raport o szkolnictwie 1',
-            value: 'Raport o szkolnictwie 1'
-        },
-        {
-            label: 'nauka: Raport o szkolnictwie 1',
-            value: 'Raport o szkolnictwie 1'
-        },
-        {
-            label: 'szkoła: Raport o szkolnictwie 1',
-            value: 'Raport o szkolnictwie 1'
-        },
-        {
-            label: 'oceny: Raport o szkolnictwie 1',
-            value: 'Raport o szkolnictwie 1'
-        },
-        {
-            label: 'podstawówka: Raport o szkolnictwie 1',
-            value: 'Raport o szkolnictwie 1'
-        },
-        {
-            label: 'szkoła średnia: Raport o szkolnictwie 1',
-            value: 'Raport o szkolnictwie 1'
-        },
-        {
-            label: 'szkolnictwo: Raport o szkolnictwie 2',
-            value: 'Raport o szkolnictwie 2'
-        },
-        {
-            label: 'uniwersytety: Raport o szkolnictwie 2',
-            value: 'Raport o szkolnictwie 2'
-        },
-        {
-            label: 'szkoła: Raport o szkolnictwie 2',
-            value: 'Raport o szkolnictwie 2'
-        },
-        {
-            label: 'studenci: Raport o szkolnictwie 2',
-            value: 'Raport o szkolnictwie 2'
-        },
-        {
-            label: 'praca: Raport o szkolnictwie 2',
-            value: 'Raport o szkolnictwie 2'
-        },
-        {
-            label: 'staże: Raport o szkolnictwie 2',
-            value: 'Raport o szkolnictwie 2'
-        },
-        {
-            label: 'rolnictwo: Raport o rolnictwie',
-            value: 'Raport o rolnictwie'
-        },
-        {
-            label: 'uprawa: Raport o rolnictwie',
-            value: 'Raport o rolnictwie'
-        },
-        {
-            label: 'uprawy: Raport o rolnictwie',
-            value: 'Raport o rolnictwie'
-        },
-        {
-            label: 'zboże: Raport o rolnictwie',
-            value: 'Raport o rolnictwie'
-        },
-        {
-            label: 'rośliny: Raport o rolnictwie',
-            value: 'Raport o rolnictwie'
-        },
-        {
-            label: 'maszyny rolnicze: Raport o rolnictwie',
-            value: 'Raport o rolnictwie'
-        },
-    ];
-};
-
-export const getKeywords = (documentName) => {
-    if (documentName === 'Raport o szkolnictwie 1') {
-        return ['szkolnictwo', 'nauka', 'szkoła', 'oceny', 'podstawówka', 'szkoła średnia'];
-    } else if (documentName === 'Raport o szkolnictwie 2') {
-        return ['szkolnictwo', 'uniwersytety', 'szkoła', 'studenci', 'praca', 'staże'];
-    } else if (documentName === 'Raport o rolnictwie') {
-        return ['rolnictwo', 'uprawa', 'uprawy', 'zboże', 'rośliny', 'maszyny rolnicze'];
-    } 
-};
