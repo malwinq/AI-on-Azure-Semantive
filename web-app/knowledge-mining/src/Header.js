34c2affb9d353cb0ba5d38b055336a57707efed1
malwinq <malwina.kubas@gmail.com>
2020-12-16 15:05:06 +0100

    Move header to separate file

---
+++ web-app/knowledge-mining/src/Header.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = {
+  root: {
+    marginTop: '5%',
+    justifyContent: 'center',
+    color: 'blue',
+    marginLeft: 'auto',
+    marginBottom: '3%'
+  },
+}
+
+class Header extends Component {
+  render() {
+    const { classes } = this.props;
+    return (
+        <Typography variant="h4" className={classes.root}>
+            Knowledge Mining Browser
+        </Typography>
+      );
+  }
+}
+
+export default withStyles(styles)(Header);
