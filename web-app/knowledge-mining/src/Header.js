7402109564c04eacbfa75845f79cb6e25e0a9267
malwinq <malwina.kubas@gmail.com>
2020-12-16 17:25:18 +0100

    Add menu

--- web-app/knowledge-mining/src/Header.js
+++ web-app/knowledge-mining/src/Header.js
@@ -1,56 +1,107 @@
 import React, { Component } from 'react';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
-import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-
-const theme = createMuiTheme({
-  typography: {
-    fontFamily: [
-      'Roboto',
-      'cursive',
-    ].join(','),
-  },});
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import SendIcon from '@material-ui/icons/Send';
+import Button from '@material-ui/core/Button';
 
 const styles = {
-  root: {
-    marginTop: '5%',
-    justifyContent: 'center',
-    color: '#1b19b6',
-    marginLeft: 'auto',
-    marginBottom: '3%'
-  },
   navBar: {
     backgroundColor: '#1b19b6'
+  },
+  menuButton: {
+    position: 'absolute',
+    bottom: '3%',
+    left: '1%'
   }
 }
+  
+// const StyledMenuItem = withStyles((theme) => ({
+//     root: {
+//       '&:focus': {
+//         backgroundColor: theme.palette.primary.main,
+//         '& .MuiListItemIcon-root, & .MuiListItemText-primary': {
+//           color: theme.palette.common.white,
+//         },
+//       },
+//     },
+//   }))(MenuItem);
 
 class Header extends Component {
-  render() {
-    const { classes } = this.props;
-    return (
-        <div>
-        <AppBar position="static" className={classes.navBar}>
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Knowledge Mining Browser
-          </Typography>
-        </Toolbar>
-      </AppBar>
-        <ThemeProvider theme={theme}>
-            <Typography variant="h5" className={classes.root}>
-                
-            </Typography>
-        </ThemeProvider>
+    state = {
+        menuOpen: false,
+        anchorEl: null
+    }
+
+    openMenu = (event) => {
+        this.setState({ menuOpen: true, anchorEl: event.currentTarget });
+    }
+
+    closeMenu = () => {
+        this.setState({ menuOpen: false });
+    }
+
+    getInfo = () => {
+
+    }
+
+    render() {
+        const { classes } = this.props;
+        return (
+            <div>
+                <AppBar position="static" className={classes.navBar}>
+                    <Toolbar>
+                        <Typography variant="h6" className={classes.title}>
+                            Knowledge Mining Browser
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+                <Button
+                    className={classes.menuButton}
+                    aria-controls="customized-menu"
+                    aria-haspopup="true"
+                    variant="contained"
+                    color="primary"
+                    onClick={this.openMenu}
+                    >Open Menu
+                </Button>
+                <Menu
+                    id="customized-menu"
+                    open={this.state.menuOpen}
+                    onClose={this.closeMenu}
+                    anchorEl={this.state.anchorEl}
+                    keepMounted={true}
+                >
+                    <MenuItem>
+                        <ListItemIcon>
+                            <SendIcon fontSize="small" />
+                        </ListItemIcon>
+                        <ListItemText primary="Sent mail" />
+                    </MenuItem>
+                    <MenuItem>
+                        <ListItemIcon>
+                            <DraftsIcon fontSize="small" />
+                        </ListItemIcon>
+                        <ListItemText primary="Drafts" />
+                    </MenuItem>
+                    <MenuItem>
+                        <ListItemIcon>
+                            <InboxIcon fontSize="small" />
+                        </ListItemIcon>
+                        <ListItemText primary="Inbox" />
+                    </MenuItem>
+            </Menu>
         </div>
-      );
-  }
+        );
+    }
 }
 
 export default withStyles(styles)(Header);
