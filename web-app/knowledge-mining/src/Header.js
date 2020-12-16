2a22825a029ccdcc22f834b47f8d1fb65769aa9d
malwinq <malwina.kubas@gmail.com>
2020-12-16 16:36:59 +0100

    Add NavBar

--- web-app/knowledge-mining/src/Header.js
+++ web-app/knowledge-mining/src/Header.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      'Roboto',
+      'cursive',
+    ].join(','),
+  },});
 
 const styles = {
   root: {
@@ -10,15 +23,32 @@ const styles = {
     marginLeft: 'auto',
     marginBottom: '3%'
   },
+  navBar: {
+    backgroundColor: '#1b19b6'
+  }
 }
 
 class Header extends Component {
   render() {
     const { classes } = this.props;
     return (
-        <Typography variant="h4" className={classes.root}>
+        <div>
+        <AppBar position="static" className={classes.navBar}>
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
             Knowledge Mining Browser
-        </Typography>
+          </Typography>
+        </Toolbar>
+      </AppBar>
+        <ThemeProvider theme={theme}>
+            <Typography variant="h5" className={classes.root}>
+                
+            </Typography>
+        </ThemeProvider>
+        </div>
       );
   }
 }
