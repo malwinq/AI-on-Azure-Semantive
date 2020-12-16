fd16a4cc7c004b66fafe4b0ea0f1e56316527767
malwinq <malwina.kubas@gmail.com>
2020-12-16 14:30:43 +0100

    Fix mock data

--- web-app/knowledge-mining/src/functions.js
+++ web-app/knowledge-mining/src/functions.js
@@ -59,7 +59,7 @@ export const getDocuments = (inputText) => {
                 title: 'Raport o szkolnictwie 2',
                 keywords: ['szkolnictwo', 'uniwersytety', 'szkoła', 'studenci', 'praca', 'staże'],
                 location: 'http://mockaddress10.com',
-                date: 'Wed Dec 06 2020 ',
+                date: 'Fri Dec 06 2020 ',
                 institution: 'USA',
                 header: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
                 type: 'wykres-liczbowy'
@@ -68,16 +68,16 @@ export const getDocuments = (inputText) => {
                 title: 'Raport o robotyce',
                 keywords: ['roboty', 'maszyny', 'automatyka', 'sterowniki', 'fabryki', 'produkcja'],
                 location: 'http://mockaddress4.com',
-                date: 'Wed Dec 01 2020 ',
+                date: 'Mon Dec 01 2020 ',
                 institution: 'EU',
                 header: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
                 type: 'tabela'
             },
             {
-                title: 'Raport o szkolnictwie 1',
+                title: 'Raport o szkolnictwie',
                 keywords: ['szkolnictwo', 'nauka', 'szkoła', 'oceny', 'podstawówka', 'szkoła średnia'],
                 location: 'http://mockaddress1.com',
-                date: 'Wed Dec 16 2020 ',
+                date: 'Thu Dec 16 2020 ',
                 institution: 'EU',
                 header: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
                 type: 'wykres-kolowy'
