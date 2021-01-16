6006049f9f824acb027ae8b4084277b8878721e8
bogaczm <m.bogacz197@gmail.com>
2021-01-16 23:29:21 +0100

    tekst

--- custom_skills/CustomSkills/CustomSkill2_HttpTrig/__init__.py
+++ custom_skills/CustomSkills/CustomSkill2_HttpTrig/__init__.py
@@ -2,23 +2,47 @@ import logging
 
 import azure.functions as func
 
+""" Adapted from the second, shorter solution at http://www.codecodex.com/wiki/Calculate_digits_of_pi#Python
+"""
+
+def pi_digits_Python(digits):
+    scale = 10000
+    maxarr = int((digits / 4) * 14)
+    arrinit = 2000
+    carry = 0
+    arr = [arrinit] * (maxarr + 1)
+    output = ""
+
+    for i in range(maxarr, 1, -14):
+        total = 0
+        for j in range(i, 0, -1):
+            total = (total * j) + (scale * arr[j])
+            arr[j] = total % ((j * 2) - 1)
+            total = total / ((j * 2) - 1)
+
+        output += "%04d" % (carry + (total / scale))
+        carry = total % scale
+
+    return output
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info('DigitsOfPi HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
+    digits_param = req.params.get('digits')
+
+    if digits_param is not None:
         try:
-            req_body = req.get_json()
+            digits = int(digits_param)
         except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+            digits = 10   # A default
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+        if digits > 0:
+            digit_string = pi_digits_Python(digits)
+
+            # Insert a decimal point in the return value
+            return func.HttpResponse(digit_string[:1] + '.' + digit_string[1:])
+
+    return func.HttpResponse(
+         "Please pass the URL parameter ?digits= to specify a positive number of digits.",
+         status_code=400
+    )
--- custom_skills/README.md
+++ custom_skills/README.md
@@ -4,11 +4,19 @@ https://docs.microsoft.com/pl-pl/azure/developer/python/tutorial-vs-code-serverl
 
 https://docs.microsoft.com/pl-pl/azure/search/search-what-is-an-index - Creating Search service and Storage service
 
+<hr> 
+
 Summary:
 
 1. Create Azure Storage with Blob Storage - import all files into the storage
 2. Create Azure Search service - inside define one index for files, and create indexer (to have full control, I did not set it for periodic use)
 3. Azure SkillSets - default skills that can be applied like finding info about people, locations, numbers and adresses.
 
-Up to this point the task was pretty straightforward and consisted mostly of following the path laid in the second attached link. Following steps are more complicated, the Custom Skills are created via the Azure Function service
+Up to this point the task was pretty straightforward and consisted mostly of following the path laid in the second attached link. Following steps are more complicated, the Custom Skills are created via the Azure Functions service.
+
+1. Install the Azure Functions Core Tools 
+2. Install VS Code
+3. Install Extensions: Azure Functions, Azure Storage
+4. Log into new project
+5. Create Http triggered function
 
