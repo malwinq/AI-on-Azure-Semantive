efbf013ce1c77c7e41108a489b3e09f9b41a4473
malwinq <malwina.kubas@gmail.com>
2021-01-27 21:18:28 +0100

    Add constants

--- web-app/knowledge-mining/src/functions.js
+++ web-app/knowledge-mining/src/functions.js
@@ -6,10 +6,12 @@ export const getDocuments = (inputText) => {
             'Content-Type': 'application/json'
         }
     };
-    const apiKey = "<API-KEY>";
+    const apiKey = "<<API_KEY>>";
     return axios.get(`https://semantive-azure-search.search.windows.net/indexes/semantive-azureblob-index/docs?api-version=2020-06-30&search=${inputText}&api-key=${apiKey}`, config);
 };
 
 export const getFile = (fileURL) => {
     return axios.get(fileURL);
 };
+
+export const FILE_TYPES = ["Image", "Complete Raport", "Editable Raport", "Calculations", "Undefined"];
